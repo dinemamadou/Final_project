25fac5f9ca19fabaecef2b00ecad34fdae9a483c
Dine <dine.mamadou@rutgers.edu>
2018-05-08 17:30:20 -0400

    3

--- scripts/scripts_shouted.R
+++ scripts/scripts_shouted.R
@@ -4,6 +4,10 @@ library(xaringan)
 library(dbplyr)
 library(readr)
 library(here)
+library(broom)
+library(knitr)
+library(MuMIn)
+library(lme4)
 
 shouted <- read_csv(here("data", "formantsdur.csv")) %>%
   mutate(., int_c = intensity - mean(intensity)) %>%
@@ -17,42 +21,156 @@ shouted1 <- shouted %>%
   separate(., TextGridLabel, into = c("word", "rep"), sep = "_") 
 
 plot1 <- shouted1 %>%
-  group_by(., gender) %>%
+  group_by(., vowel) %>%
   ggplot(., aes(x = F2_midpoint, y = F1_midpoint, 
-                color = condition, label = vowel)) +
-  geom_point() +
-  geom_label()
+                color = condition)) +
+  geom_point() 
 
 plot2 <- shouted1 %>%
+  ggplot(., aes(x = dur_c, y = int_c, color = condition)) +
+    facet_grid(. ~ gender) +
+    geom_boxplot() +
+    scale_color_brewer(palette = "Set1", name = "condition") +
+     labs(x = "Centered duration (ms)", 
+          y = "Centered intensidty (dB)", 
+       title = "Intensity by duration", 
+       caption = "Mean +/- SE")
+
+#save1 <- ggsave(filename = "./figs/durByInt.png",
+#       plot = plot2,
+#       width = 9, height = 4,
+#       unit = "in") #to save the plot in a folder of our choice
+
+
+plot3 <- shouted1 %>%
   ggplot(., aes(x = dur_c, y = int_c, color = condition, shape = gender)) +
-  geom_point() 
-  
+    geom_point() +
+    scale_color_brewer(palette = "Set1", name = "condition") +
+     labs(x = "Centered duration (ms)", 
+          y = "Centered intensidty (dB)", 
+       title = "Intensity by duration", 
+       caption = "Mean +/- SE")
+
+plot4 <- shouted1 %>%
+  ggplot(., aes(x = int_c, y = f1_c, color = condition, shape = gender)) +
+    geom_point() +
+    scale_color_brewer(palette = "Set1", name = "condition") +
+     labs(x = "Centered intensidty (dB)", 
+          y = "Centered F1 (Hz)", 
+       title = "F1 as a Function of Intensity", 
+       caption = "Mean +/- SE")
+
+#ggsave(filename = "./figs/intByF1.png",
+#       plot = plot4,
+#       width = 9, height = 4,
+#       unit = "in")
+
 
 #F1 ~
 
 
 # 2. check structure
-head(shouted1)
-#summary(poisson)
-# 3. fit inclusive and nested models
+head(shouted1)  
+#lme models
 
-mod_null <- glm(F1_midpoint ~ 1, data = shouted1,
+mod_f1 <- lmer(f1_c ~ int_c + dur_c + gender + f2_c + int_c:gender +
+          (1 | gender) + (1 + rep | word), 
+           data = shouted1, REML = F)
+summary(mod_f1)
+
+mod_f2 <- lmer(f2_c ~ int_c + dur_c + gender + f1_c + int_c:gender + 
+          (1 | gender) + (1 + rep | word), 
+           data = shouted1, REML = F)
+summary(mod_f2)
+
+
+mod_dur <- lmer(dur_c ~ int_c + f1_c + gender + f2_c + int_c:gender + 
+          (1 | gender) + (1 + rep | word), 
+           data = shouted1, REML = F)
+
+summary(mod_dur)
+
+d <- anova(mod_f1, mod_f2, mod_dur)
+
+dur_resid <- plot(fitted(mod_dur), residuals(mod_dur))
+
+
+#homoskedastic
+
+dur_res1 <- qqnorm(residuals(mod_dur))
+dur_res2 <- qqline(residuals(mod_dur))
+
+
+
+# 3. Nested models with f1 as the criterion
+
+mod_null <- glm(f1_c ~ 1, data = shouted1,
                   family = gaussian(link = "identity"))
 
-mod_int_c <- glm(F1_midpoint ~ int_c, 
+mod_int_c <- glm(f1_c ~ 1 + int_c, 
                  data = shouted1, family = gaussian(link = "identity"))
 
-mod_inter1 <- glm(F1_midpoint ~ int_c * gender, 
+mod3 <- glm(f1_c ~ 1 + int_c + gender, 
                  data = shouted1, family = gaussian(link = "identity"))
 
-mod_inter2 <- glm(F1_midpoint ~ int_c * dur_c, 
+mod4 <- glm(f1_c ~ 1 + int_c + gender + dur_c, 
                  data = shouted1, family = gaussian(link = "identity"))
 
-mod_full1 <- glm(F1_midpoint ~ int_c * dur_c * gender, 
-                data = shouted1, family = gaussian(link = "identity"))
+mod5 <- glm(f1_c ~ 1 + int_c + gender + dur_c + int_c:gender, 
+                 data = shouted1, family = gaussian(link = "identity"))
+
+mod_full <- glm(f1_c ~ 1 + int_c + gender + dur_c + int_c:gender + dur_c:gender, 
+                 data = shouted1, family = gaussian(link = "identity"))
+
+anova_f1 <- anova(mod_null, mod_int_c, mod3, mod4, mod5, mod_full, test = "Chisq")
+
+summary(anova_f1)
+r.squaredGLMM(mod5) #for r-squared
+
+# Nested models with f2 as the criterion (no effect whatsoever, 
+# so f2 will be removed from all models)
 
 
-summary(mod_full1)
+# Nested models with durationas the criterion
+
+mod_null2 <- glm(dur_c ~ 1, data = shouted1,
+                  family = gaussian(link = "identity"))
+
+mod_int_c2 <- glm(dur_c ~ 1 + int_c, 
+                 data = shouted1, family = gaussian(link = "identity"))
+
+mod32 <- glm(dur_c ~ 1 + int_c + gender, 
+                 data = shouted1, family = gaussian(link = "identity"))
+
+mod42 <- glm(dur_c ~ 1 + int_c + gender + f1_c, 
+                 data = shouted1, family = gaussian(link = "identity"))
+
+
+mod52 <- glm(dur_c ~ 1 + int_c + gender + f1_c + int_c:gender, 
+                 data = shouted1, family = gaussian(link = "identity"))
+
+mod_full2 <- glm(dur_c ~ 1 + int_c + gender + f1_c + int_c:gender + f1_c:gender, 
+                 data = shouted1, family = gaussian(link = "identity"))
+
+summary(mod32)
+r.squaredGLMM(mod52) #for r-squared
+
+
+broom::tidy(mod32) %>% knitr::kable(., digits = 2)
+
+anova_dur <- anova(mod_null2, mod_int_c2, mod32, mod42, mod52, mod_full2, test = "Chisq") %>% 
+  knitr::kable(., format = 'latex', digits = 2)
+
+
+dur_resid <- plot(fitted(mod_full2), residuals(mod_full2))
+#homoskedastic
+
+dur_res1 <- qqnorm(residuals(mod_full2))
+dur_res2 <- qqline(residuals(mod_full2))
+
+
+
+r.squaredGLMM(mod_full) #for r-squared
 
 MainEffect <- matrix(c(662.89,24.75,26.78,2e-16,-3.4,10.56,-0.32,0.75,455.60,222.72,2.05,0.04,-127.95,125.29,-1.02,0.31),ncol=4,byrow=TRUE)
  colnames(MainEffect) <- c("estimate","std.Err","statistics","p-value")
@@ -62,9 +180,7 @@ MainEffect <- matrix(c(662.89,24.75,26.78,2e-16,-3.4,10.56,-0.32,0.75,455.60,222
 #Main effect of duration
 
 # to avoid a Type I error, I removed gender and condition as predictors.
-models <- anova(mod_null, mod_inter1, mod_inter2, mod_full1, test = "Chisq")
 
-summary(models)
 
 
 
@@ -74,9 +190,14 @@ summary(models)
 group <- shouted1 %>% 
   group_by(., condition) %>%
   summarize(., meanf1 = mean(F1_midpoint),
+            sd.f1 = sd(F1_midpoint),
             mean.int = mean(intensity),
-            mean.dur = mean(duration)) %>%
-  knitr::kable(., format = 'html', digits = 2)
+            sd.int = sd(intensity),
+            mean.dur = mean(duration),
+            sd.dur = sd(duration))%>%
+  knitr::kable(., format = 'latex', digits = 2)
+
+
 
 #test for interactions/
 #anova <- anova(model_null, model_p, model_add, model_int, test = "Chisq") #Chisq includs the p value
